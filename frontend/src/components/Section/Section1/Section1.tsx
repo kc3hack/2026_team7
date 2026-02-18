827871b09077eb479612b4ab4f900652c72a39e5
soshi-1982 <shuangshigongtian@gmail.com>
2026-02-18 21:08:08 +0900

    create Section1 styles

--- frontend/src/components/Section/Section1/Section1.tsx
+++ frontend/src/components/Section/Section1/Section1.tsx
@@ -1,7 +1,50 @@
 import './Section1.css';
 
 const Section1 = () => {
-  return <h1>Section 1</h1>;
+  return  <div className="card">
+            <div className="left">
+              <img src="/engineer.jpg" alt="engineer" />
+            </div>
+
+            <div className="right">
+
+              <div className="box name-box">
+                <div className="top-row">
+                  <span>エンジニア名</span>
+                  <span className="id">ID : yutota13</span>
+                </div>
+                <div className="name">ゆーと</div>
+                <button className="qr-btn">QR表示</button>
+              </div>
+
+              <div className="middle-row">
+                <div className="box history-box">
+                  <div className="history-header">
+                    <div className="Registration">エンジニア歴</div>
+                    <div className="Registration-date">登録日：2019/03/10</div>
+                  </div>
+
+                  <div className="big">6年</div>
+                </div>
+
+                <div className="box level-box">
+                  <div className="level-title">LV</div>
+                  <div className="level">120</div>
+                </div>
+              </div>
+
+              <div className="box title-box">
+                <div className="title">称号</div>
+                <div className="title-neme">暁のエンジニア</div>
+                <button className="update-btn">スキル更新</button>
+              </div>
+
+              <div className="updated">
+                スキル更新日時：2026.02.17 05:43:32
+              </div>
+
+            </div>
+          </div>;
 };
 
 export default Section1;
