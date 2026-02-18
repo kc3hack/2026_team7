6fe9008433319eac944b0521558027d6834da7de
e1q23079 <e1q23079@oit.ac.jp>
2026-02-18 21:35:26 +0900

    Updateページの追加とスタイルの適用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Top from './pages/Top/Top';
+import Update from './pages/Update/Update';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Top />} />
+      <Route path="/update" element={<Update />} />
     </Routes>
   );
 }
--- frontend/src/pages/Update/Update.tsx
+++ frontend/src/pages/Update/Update.tsx
@@ -1,9 +1,19 @@
 import './Update.css';
+import BoxFrame from '../../components/BoxFrame/BoxFrame';
+import Status from '../../components/Status/Status';
+import UpdateBtn from '../../components/UpdateBtn/UpdateBtn';
 
 const Update = () => {
   return (
-    <div>
-      <h1>Update</h1>
+    <div className="page">
+      <BoxFrame>
+        <div className="container">
+          <h1 className="title">Skill Update</h1>
+          <Status />
+          <p className="user-id">ID : yutota13</p>
+          <UpdateBtn />
+        </div>
+      </BoxFrame>
     </div>
   );
 };
