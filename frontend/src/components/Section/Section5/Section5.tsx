bc91c289cf381ce1acab1c7a4546180d54de7bba
e1q23079 <e1q23079@oit.ac.jp>
2026-02-18 17:27:10 +0900

    feat: Section5 コンポーネントを作成し、スキルセクションを追加

--- frontend/src/components/Section/Section5/Section5.tsx
+++ frontend/src/components/Section/Section5/Section5.tsx
@@ -1,7 +1,24 @@
 import './Section5.css';
+import BoxFrame from '../../BoxFrame/BoxFrame';
+import LanguageSkill from '../../LanguageSkill/LanguageSkill';
 
 const Section5 = () => {
-  return <h1>Section 5</h1>;
+  return (      
+    <BoxFrame>
+      <div className="skills">
+        <h2 className="skills-title">
+          Programming Skills by Language
+        </h2>
+
+        <LanguageSkill />
+        <LanguageSkill  />
+        <LanguageSkill  />
+        <LanguageSkill  />
+        <LanguageSkill  />
+        <LanguageSkill  />
+      </div>
+    </BoxFrame>
+  );
 };
 
 export default Section5;
