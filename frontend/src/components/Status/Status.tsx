9e354d0d292ff423b7a3466af4d1fdff3bf00bc4
soshi-1982 <shuangshigongtian@gmail.com>
2026-02-18 21:48:32 +0900

    create status styles

--- frontend/src/components/Status/Status.tsx
+++ frontend/src/components/Status/Status.tsx
@@ -1,7 +1,14 @@
 import './Status.css';
+import img1 from "../assets/img/not-updated.png";
+import img2 from "../assets/img/updating.png";
+import img3 from "../assets/img/updated.png";
 
 const Status = () => {
-  return <h1>Status</h1>;
+  return <div>
+            <img src={img1} alt="img1" />
+            <img src={img2} alt="img2" />
+            <img src={img3} alt="img3" />
+          </div>;
 };
 
 export default Status;
