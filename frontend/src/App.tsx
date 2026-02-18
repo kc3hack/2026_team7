588cf6f67d19184f6d5b1cca4d3fe1ed455f9538
e1q23079 <e1q23079@oit.ac.jp>
2026-02-18 20:17:25 +0900

    カードページにセクションコンポーネントを追加し、BodyFrameでラップしました。Appコンポーネントにルートを追加しました。スタイルを調整しました。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Top from './pages/Top/Top';
+import Card from './pages/Card/Card';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Top />} />
+      <Route path="/card" element={<Card />} />
     </Routes>
   );
 }
--- frontend/src/pages/Card/Card.tsx
+++ frontend/src/pages/Card/Card.tsx
@@ -1,9 +1,21 @@
 import './Card.css';
+import BodyFrame from '../../components/BodyFrame/BodyFrame';
+import Section1 from '../../components/Section/Section1/Section1';
+import Section2 from '../../components/Section/Section2/Section2';
+import Section3 from '../../components/Section/Section3/Section3';
+import Section4 from '../../components/Section/Section4/Section4';
+import Section5 from '../../components/Section/Section5/Section5';
 
 const Card = () => {
   return (
     <div>
-      <h1>Card</h1>
+      <BodyFrame>
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />   
+      </BodyFrame>
     </div>
   );
 };
