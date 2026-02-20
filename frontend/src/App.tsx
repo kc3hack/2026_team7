967c2806507be65ce7772517906f9973eda1b726
kanayama-hiroki <gourudomounten@gmail.com>
2026-02-20 15:20:26 +0900

    ルーティングパスを修正し、カードページと更新ページのパスを動的に取得できるようにしました

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,15 @@ import Update from './pages/Update/Update';
 function App() {
   return (
     <Routes>
+      {/* トップページ：/ */}
       <Route path="/" element={<Top />} />
-      <Route path="/card" element={<Card />} />
-      <Route path="/update" element={<Update />} />
+      
+      {/* カードページ：/cards/{user_name}/ */}
+      {/* :user_name とすることで、URLから名前を動的に取得できます */}
+      <Route path="/cards/:user_name" element={<Card />} />
+      
+      {/* 更新画面ページ：/cards/{user_name}/update */}
+      <Route path="/cards/:user_name/update" element={<Update />} />
     </Routes>
   );
 }
